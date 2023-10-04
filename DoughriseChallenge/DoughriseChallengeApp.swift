aa931405d008406e6750456d4558e05784d41db3
David Leyva <dleyvaabrahantes@gmail.com>
2023-10-04 11:50:51 -0400

    Initial Commit

---
+++ DoughriseChallenge/DoughriseChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughriseChallengeApp.swift
+//  DoughriseChallenge
+//
+//  Created by David on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DoughriseChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
